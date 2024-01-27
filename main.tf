41f5a18ea806b4d8292ce038e68a3bb5d1350b55
coolkiddzzz <coolkiddzzz@test.com>
2024-01-27 10:36:01 +0800

    commit sns terraform

---
+++ main.tf
@@ -0,0 +1,17 @@
+
+terraform {
+  backend "s3" {
+    bucket = "sctp-ce5-tfstate-bucket-1"
+    key    = "wy.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_sns_topic" "user_updates" {
+  name = "waiyee-sns"
+}
