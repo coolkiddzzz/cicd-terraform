2ab9fd214e6ce11f725a7263f62106eb5064aa1a
coolkiddzzz <coolkiddzzz@test.com>
2024-01-30 12:19:52 +0800

    added new s3 bucket

--- main.tf
+++ main.tf
@@ -14,3 +14,11 @@ provider "aws" {
 resource "aws_sns_topic" "user_updates" {
   name = "waiyee-sns-test-branch"
 }
+
+resource "aws_s3_bucket" "new_bucket" {
+  bucket = "wy-tf-bucket"
+
+  tags = {
+    Name        = "wy-bucket"
+  }
+}
