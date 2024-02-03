5c06922032ad75af033a3d7c1ab6361bbd3e9293
coolkiddzzz <coolkiddzzz@test.com>
2024-02-03 17:09:05 +0800

    debug group id

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "aws_instance" "wy-webserver2" {
   instance_type = "t2.micro"
   associate_public_ip_address = "true"
   subnet_id = "subnet-0d647d78d2309afc0"
-  vpc_security_group_ids = "sg-0044a66b4c899960b"
+  vpc_security_group_ids = ["sg-0044a66b4c899960b"]
   tags = {
     Name = "waiyee-webserver"
   }
@@ -51,7 +51,7 @@ resource "aws_instance" "wy-ans" {
   instance_type = "t2.micro"
   associate_public_ip_address = "true"
   subnet_id = "subnet-0d647d78d2309afc0"
-  vpc_security_group_ids = "sg-0044a66b4c899960b"
+  vpc_security_group_ids = ["sg-0044a66b4c899960b"]
   tags = {
     Name = "waiyee-webserver"
   }
