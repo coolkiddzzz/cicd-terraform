c4351c33306d2781db6890ae8d715d38ecb415c8
coolkiddzzz <coolkiddzzz@test.com>
2024-02-03 17:10:39 +0800

    edited for debug grp id

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "aws_instance" "wy-webserver1" {
   instance_type = "t2.micro"
   associate_public_ip_address = "true"
   subnet_id = "subnet-0d647d78d2309afc0"
-  vpc_security_group_ids = "sg-0044a66b4c899960b"
+  vpc_security_group_ids = ["sg-0044a66b4c899960b"]
   tags = {
     Name = "waiyee-webserver"
   }
