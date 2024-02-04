44ba0af7ac071a8e8d5c9721571a25a6033df3e7
coolkiddzzz <coolkiddzzz@test.com>
2024-02-04 11:46:25 +0800

    ec2 key

--- main.tf
+++ main.tf
@@ -24,35 +24,20 @@ resource "aws_s3_bucket" "new_bucket" {
   }
 }
 
-resource "aws_instance" "wy-webserver1" {
-  ami           = "ami-0e9107ed11be76fde"
-  instance_type = "t2.micro"
-  associate_public_ip_address = "true"
-  subnet_id = "subnet-0d647d78d2309afc0"
-  vpc_security_group_ids =  ["sg-035b6442f9f859979"]
-  tags = {
-    Name = "waiyee-webserver"
-  }
+resource "tls_private_key" "coolkidz-key-test" {
+  algorithm   = "ECDSA"
+  ecdsa_curve = "P384"
 }
 
-resource "aws_instance" "wy-webserver2" {
-  ami           = "ami-0e9107ed11be76fde"
-  instance_type = "t2.micro"
-  associate_public_ip_address = "true"
-  subnet_id = "subnet-0d647d78d2309afc0"
-  vpc_security_group_ids =  ["sg-035b6442f9f859979"]
-  tags = {
-    Name = "waiyee-webserver"
-  }
-}
 
-resource "aws_instance" "wy-ans" {
+resource "aws_instance" "webserver" {
   ami           = "ami-0e9107ed11be76fde"
   instance_type = "t2.micro"
   associate_public_ip_address = "true"
   subnet_id = "subnet-0d647d78d2309afc0"
   vpc_security_group_ids =  ["sg-035b6442f9f859979"]
+  key_name = 'coolkidz-key-test'
   tags = {
-    Name = "waiyee-webserver"
+    Name = "waiyee-webserver1"
   }
 }
