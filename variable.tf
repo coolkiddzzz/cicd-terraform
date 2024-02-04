4e2ca9303e808742578f538c88226b163baa0719
coolkiddzzz <coolkiddzzz@test.com>
2024-02-04 12:06:20 +0800

    edit variable file

--- variable.tf
+++ variable.tf
@@ -1,11 +1,12 @@
 variable "subnet_id" {
   description = "Subnet ID"
   type        = string
-  sensitive   = true
+  default     = "subnet-0d647d78d2309afc0"
+
 }
 
 variable "vpc_security_group_ids" {
   description = "VPC Security group ID"
   type        = list
-  sensitive   = true
+  default = ["sg-035b6442f9f859979"]
 }
