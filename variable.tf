ce88d6a075812acef278a9767d34041c1ef71bb7
coolkiddzzz <coolkiddzzz@test.com>
2024-02-04 12:31:43 +0800

    .gitignore file

---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "subnet_id" {
+  description = "Subnet ID"
+  type        = string
+  default     = "subnet-0d647d78d2309afc0"
+
+}
+
+variable "vpc_security_group_ids" {
+  description = "VPC Security group ID"
+  type        = list
+  default = ["sg-035b6442f9f859979"]
+}
+
+variable "key_name" {
+  description = "Key name for ec2"
+  type        = string
+  default = "wy-useast1-keypair"
+
+}
